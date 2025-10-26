a38efa960688e11382b33bd11a01d0109975f66b
kholod ali <kholod27112003@gmail.com>
2025-10-26 08:30:02 -0700

    start NewsApp

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/utils/app_routes.dart';
+
+void main() {
+  runApp(const NewsApp());
+}
+
+class NewsApp extends StatelessWidget {
+  const NewsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: AppRoutes.homeRouteName,
+      routes: {AppRoutes.homeRouteName: (context) => HomeScreen()},
+    );
+  }
+}
